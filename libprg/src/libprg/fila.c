0b998f276e5c4f93dfdcb508a75c5952237aff28
aluno <nicolas.s09@aluno.ifsc.edu.br>
2026-03-26 16:59:43 -0300

    começo da fila.c

---
+++ libprg/src/libprg/fila.c
@@ -0,0 +1,45 @@
+//
+// Created by aluno on 26/03/2026.
+//
+
+#include <stdlib.h>
+
+typedef struct fila {
+    int* elementos;
+    int inicio;
+    int fim;
+    int tamanho;
+    int capacidade;
+}fila_t;
+
+fila_t* cria_fila(int capacidade) {
+
+    fila_t* fila = (fila_t*) malloc(sizeof(fila_t));
+    fila->elementos = malloc(sizeof(int) * capacidade);
+    fila->inicio = 0;
+    fila->fim = 0;
+    fila->tamanho = 0;
+    fila->capacidade = capacidade;
+
+    return fila;
+}
+
+int destruir_fila_fila(fila_t* fila) {
+
+    free(fila->elementos);
+    free(fila);
+    return 0;
+
+}
+void enfileirar(fila_t* fila, int valor) {
+
+    fila->elementos[fila->fim] = valor;
+    fila->fim = (fila->fim + 1) % fila->capacidade;
+    fila->tamanho++;
+}
+// desenfileirar
+// inicio
+// fim
+// vazia
+// cheia
+
